242132c6d1e041f4ccbb1b6df6b7018ac69ec1be
d-shimiz <d-shimiz@users.noreply.github.com>
2018-01-13 23:39:42 +0900

    Modify '7.3 Unsuccessful signups'

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   #get 'user/new'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 end
