41b442211870395a5442271f9863a60766a654dc
Daisuke Shimizu <shimizu.daisuke.sd@gmail.com>
2018-05-13 16:09:58 +0900

    Modify '10.3.1 Users index'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:two)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
