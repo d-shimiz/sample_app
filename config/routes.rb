f7e01f3d76b8334ee186cf606c4fbe9b147ec749
Daisuke Shimizu <shimizu.daisuke.sd@gmail.com>
2018-07-16 23:58:11 +0900

    Modify '11.1 Account activations resource'

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-
-  #get 'sessions/new'
-
-  #get 'users/new'
-
   root 'static_pages#home'
-  #get 'static_pages/help'
   get '/help', to: 'static_pages#help'
-  #get 'static_pages/about'
   get '/about', to: 'static_pages#about'
-  #get 'static_pages/contact'
   get '/contact', to: 'static_pages#contact'
-  #get 'user/new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
