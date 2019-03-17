d224a5dc4ebaf35c0c5d2056b83a6bda328416be
daisukeshimizu <shimizu.daisuke.sd@gmail.com>
2019-03-17 13:50:17 +0900

    change 14.1 The Relationship model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,15 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    one = users(:one)
+    two = users(:two)
+    assert_not one.following?(two)
+    one.follow(two)
+    assert one.following?(two)
+    assert two.followers.include?(one)
+    one.unfollow(two)
+    assert_not one.following?(two)
+  end
 end
